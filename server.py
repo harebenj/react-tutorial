c415d49a870a170ca3b0ebb2a51b8de444ce6f07
Ben Hare <bhare@waveapps.com>
2015-10-25 14:28:49 -0400

    moved over to doing todos now

--- server.py
+++ server.py
@@ -15,19 +15,19 @@ from flask import Flask, Response, request
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-@app.route('/api/comments', methods=['GET', 'POST'])
-def comments_handler():
+@app.route('/api/todos', methods=['GET', 'POST'])
+def todos_handler():
 
-    with open('comments.json', 'r') as file:
-        comments = json.loads(file.read())
+    with open('todos.json', 'r') as file:
+        todos = json.loads(file.read())
 
     if request.method == 'POST':
-        comments.append(request.form.to_dict())
+        todos.append(request.form.to_dict())
 
-        with open('comments.json', 'w') as file:
-            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+        with open('todos.json', 'w') as file:
+            file.write(json.dumps(todos, indent=4, separators=(',', ': ')))
 
-    return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+    return Response(json.dumps(todos), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT",3000)))
